c6a43f5da16086435ef6d04e7fbbf33c29d6c955
BentesDev <vitorsbentes@gmail.com>
2025-04-20 12:06:17 -0300

    Camada xadrez(Enum:color e Classe:PartidaXadrez, PecaXadrez) e interface(Classe:UI)

---
+++ src/xadrez/PecaXadrez.java
@@ -0,0 +1,22 @@
+package xadrez;
+
+import jogotabuleiro.Peca;
+import jogotabuleiro.Tabuleiro;
+
+public class PecaXadrez extends Peca {
+
+	private Color color;
+
+	public PecaXadrez(Tabuleiro tabuleiro, Color color) {
+		super(tabuleiro);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	
+	
+	
+}
